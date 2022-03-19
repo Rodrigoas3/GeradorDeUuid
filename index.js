7726f18cecc0beac716c8b3930ba4c11966dd130
Rodrigoas3 <rodrigodevsilva@gmail.com>
2022-03-19 18:20:24 -0300

    Code0001-Backend-structure-for-id-generation

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser =  require('body-parser')
+const userRoutes = require('./routes/userRoutes')
+
+const app = express()
+const port = 3000
+app.use(bodyParser.urlencoded ({extended: false}))
+
+userRoutes(app)
+
+app.get('/', (req, res)=> res.send('Ola mundo com express!'))
+
+
+app.listen(port, () => console.log('Api rodando na porta 3000'))
